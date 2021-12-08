489d43f25e1345290a7bf07878788ad3a58cf8bf
Andrii Ivanenko <andrii_ivanenko1@epam.com>
2021-12-08 14:19:49 +0200

    Revert "Initialized two member variables of the NumberHolder class"
    This reverts commit 3bfdd092

--- src/main/java/javaBasicPart2/Main.java
+++ src/main/java/javaBasicPart2/Main.java
@@ -1,6 +1,6 @@
 package javaBasicPart2;
 
-public class Main extends NumberHolder{
+public class Main {
     public static void main(String[] args) {
 //  1. Write a Java program to concatenate a given string to the end of another string.
 //  And remove all `P` or `p` characters.
@@ -10,14 +10,7 @@ public class Main extends NumberHolder{
 
         String str3 = str1.concat(str2).replace("P", "");
         System.out.println(str3);
-
-//  4. Given the following class, called NumberHolder, write some code that creates an instance of the class,
-//  initializes its two member variables, and then displays the value of each member variable.
-        NumberHolder numberHolder = new NumberHolder();
-        numberHolder.aFloat = 3.75f;
-        numberHolder.anInt = 12;
-
-        System.out.println(numberHolder.aFloat);
-        System.out.println(numberHolder.anInt);
     }
+
+
 }
