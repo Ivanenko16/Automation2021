73287dfc8fde01fa42652902c5d93808443dd4af
Andrii Ivanenko <andrii_ivanenko1@epam.com>
2021-12-16 15:15:04 +0200

    Added Task1Tests

---
+++ src/test/java/javaOOP/Task1Tests.java
@@ -0,0 +1,21 @@
+package javaOOP;
+
+import javaOOP.task1.Vector;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Task1Tests {
+
+    Vector vector1 = new Vector(1, 2, 2);
+    Vector vector2 = new Vector(2, 1, 2);
+
+    @Test
+    public void checkLengthVector() {
+        Assertions.assertEquals(3, vector1.lengthVector());
+    }
+
+    @Test
+    public void checkScalarMultiplication() {
+        Assertions.assertEquals(8, Vector.scalarMultiplication(vector1, vector2));
+    }
+}
